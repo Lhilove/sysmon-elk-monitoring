62f439395d42677a4028dcecb885034dfbf737b8
Adewole Love Oluwapelumi <114921921+Lhilove@users.noreply.github.com>
2025-06-11 17:55:58 +0100

    Add files via upload

---
+++ ELK SIEM.ps1
@@ -0,0 +1,9 @@
+﻿##Elasticsearch
+cd C:\Users\Oluwpelumi\Desktop\elasticsearch-9.0.1-windows-x86_64\elasticsearch-9.0.1\bin
+.\elasticsearch.bat
+##Reset elastic password
+.\elasticsearch-reset-password.bat -u elastic
+
+##Kibana
+cd C:\Users\Oluwpelumi\Desktop\kibana-9.0.1-windows-x86_64\kibana-9.0.1\bin
+.\kibana.bat
---
+++ winlogbeat.ps1
@@ -0,0 +1,7 @@
+﻿cd C:\Users\Oluwpelumi\Desktop\winlogbeat-9.0.1-windows-x86_64\winlogbeat-9.0.1-windows-x86_64
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+.\install-service-winlogbeat
+
+Start-Service winlogbeat
